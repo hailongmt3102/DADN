efd8740415347690c80bcd4c2d64ad4fa22a054e
rongqui <rongmaphunkhoi@gmail.com>
2021-05-15 14:31:56 +0700

    viewww

--- react_native_frontend/App.js
+++ react_native_frontend/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, Image, StyleSheet, Dimensions, Text } from 'react-native';
 import Welcome from './src/screens/welcome';
-
+import Login from './src/screens/login';
 const App = () => {
   return(
     <View>
-      <Welcome/>
+      <Login/>
     </View>
   );
 }
