eed730e80cd8ac332dfcb32342bb5323586e1c3e
hailongmt3102@gmail.com <hailongmt3102@gmail.com>
2021-05-15 15:06:33 +0700

    add some screens for login app

--- react_native_frontend/App.js
+++ react_native_frontend/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Image, StyleSheet, Dimensions, Text } from 'react-native';
 import Welcome from './src/screens/welcome';
+import Login1 from './src/screens/forgotPass';
 
 const App = () => {
   return(
     <View>
       <Welcome/>
+      {/* <Login1/> */}
     </View>
   );
 }
