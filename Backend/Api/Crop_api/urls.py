4173b0dcfb0457baada514a18b3556752dde9478
NQLong <nqlong0709@gmail.com>
2021-06-12 01:58:12 +0700

    fix create crop url

--- Backend/Api/Crop_api/urls.py
+++ Backend/Api/Crop_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CropsBaseView
+from .views import CropsBaseView, CropCreateView
 
 urlpatterns = [
     path('<int:crop_id>/', CropsBaseView.as_view()),
+    path('create/', CropCreateView.as_view()),
     
 ]
